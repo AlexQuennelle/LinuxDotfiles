041bad346f02f80c5b0887f754c973f93658a86f
Alex Quennelle <bluephoenixcg@gmail.com>
2025-06-03 21:58:38 -0500

    Hide tab bar

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,11 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
-config.color_scheme = 'carbonfox'
+config.color_scheme = "carbonfox"
+config.enable_tab_bar = false
 
 return config
