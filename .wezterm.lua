7f0cdacd48096885a0ba9f225097317811e6384c
Alex Quennelle <BluePhoenixCG@gmail.com>
2025-06-07 17:59:22 -0500

    Change font weight to bold

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,10 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
+config.font = wezterm.font "JetBrains Mono Bold"
 config.color_scheme = "carbonfox"
 config.enable_tab_bar = false
+config.window_background_opacity = 0.85
+config.text_background_opacity = 1.0
 
 return config
